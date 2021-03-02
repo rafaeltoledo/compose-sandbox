aa92353793d4cd2e6c97abd57456a4c3b8a0cb63
Rafael Toledo <rafaeldtoledo@gmail.com>
2021-03-02 01:00:39 -0300

    Basic listing

--- app/src/main/java/net/rafaeltoledo/sandbox/ui/screen/repositories/RepositoriesScreen.kt
+++ app/src/main/java/net/rafaeltoledo/sandbox/ui/screen/repositories/RepositoriesScreen.kt
@@ -1,24 +1,33 @@
 package net.rafaeltoledo.sandbox.ui.screen.repositories
 
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import dev.chrisbanes.accompanist.insets.statusBarsPadding
+import androidx.lifecycle.viewmodel.compose.viewModel
 
 @Composable
-fun RepositoriesScreen(navController: NavController, viewModel: RepositoriesViewModel) {
-  val state = viewModel.state.collectAsState()
+fun RepositoriesScreen(navController: NavController, viewModel: RepositoriesViewModel = viewModel()) {
+  val state = viewModel.state.collectAsState().value
 
   Scaffold(
     modifier = Modifier
       .statusBarsPadding()
   ) { paddingValues ->
-    Text(
-      text = "${state.value.isLoading} | ${state.value.users.size}",
-    )
+    if (state.isLoading) {
+      CircularProgressIndicator()
+    } else {
+      LazyColumn(content = {
+        items(state.users) { user ->
+          Text(user.displayName)
+        }
+      })
+    }
   }
 }
